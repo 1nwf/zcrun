a7c7445a40d98988c5107aaf35c922337ff02586
Nawaf <nwf.aloufi@gmail.com>
2024-03-07 21:42:17 -0500

    (rtnetlink) only set EXCL flag on create

--- src/rtnetlink/link/link.zig
+++ src/rtnetlink/link/link.zig
@@ -42,9 +42,9 @@ const RequestType = enum {
     }
 
     fn getFlags(self: RequestType) u16 {
-        var flags: u16 = linux.NLM_F_REQUEST | linux.NLM_F_EXCL | linux.NLM_F_ACK;
+        var flags: u16 = linux.NLM_F_REQUEST | linux.NLM_F_ACK;
         switch (self) {
-            .create => flags |= linux.NLM_F_CREATE,
+            .create => flags |= linux.NLM_F_CREATE | linux.NLM_F_EXCL,
             else => {},
         }
 
