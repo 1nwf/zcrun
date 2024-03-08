d9486582ea9d950bd56a896c289416591221e8ac
Nawaf <nwf.aloufi@gmail.com>
2024-03-07 22:29:27 -0500

    (rtnetlink) update send/recv

--- src/rtnetlink/rtnetlink.zig
+++ src/rtnetlink/rtnetlink.zig
@@ -11,9 +11,8 @@ pub fn init(allocator: std.mem.Allocator) !Self {
     const fd: i32 = @intCast(linux.socket(linux.AF.NETLINK, linux.SOCK.RAW, linux.NETLINK.ROUTE));
     const kernel_addr = linux.sockaddr.nl{ .pid = 0, .groups = 0 };
     const res = linux.bind(fd, @ptrCast(&kernel_addr), @sizeOf(@TypeOf(kernel_addr)));
-    switch (linux.getErrno(res)) {
-        .SUCCESS => {},
-        else => return error.BindFailed,
+    if (linux.getErrno(res) != .SUCCESS) {
+        return error.BindFailed;
     }
 
     return .{ .allocator = allocator, .fd = fd };
@@ -23,13 +22,19 @@ pub fn deinit(self: *Self) void {
     std.os.close(self.fd);
 }
 
-pub fn exec(self: *Self, msg: []const u8) !void {
+pub fn send(self: *Self, msg: []const u8) !void {
     std.debug.assert(try std.os.send(self.fd, msg, 0) == msg.len);
-    try self.recv();
 }
 
-pub fn recv(self: *Self) !void {
-    // TODO: improve response handling
+pub fn recv(self: *Self, buff: []u8) !usize {
+    const n = try std.os.recv(self.fd, buff, 0);
+    if (n == 0) {
+        return error.InvalidResponse;
+    }
+    return n;
+}
+
+pub fn recv_ack(self: *Self) !void {
     var buff: [512]u8 = std.mem.zeroes([512]u8);
     const n = try std.os.recv(self.fd, &buff, 0);
     if (n == 0) {
@@ -41,14 +46,20 @@ pub fn recv(self: *Self) !void {
     if (header.type == .DONE) {
         return;
     } else if (header.type == .ERROR) { // ACK/NACK response
-        const err_val: *i32 = @alignCast(@ptrCast(&buff[@sizeOf(linux.nlmsghdr)]));
-        const code: linux.E = @enumFromInt(-1 * err_val.*);
-        if (code != .SUCCESS) {
-            log.info("err: {}", .{code});
-            return error.Error;
-        }
-        return;
+        const response = std.mem.bytesAsValue(NlMsgError, buff[0..]);
+        return handle_ack(response.*);
     }
+}
 
-    try self.recv();
+pub const NlMsgError = struct {
+    hdr: linux.nlmsghdr,
+    err: i32,
+    msg: linux.nlmsghdr,
+};
+
+pub fn handle_ack(msg: NlMsgError) !void {
+    const code: linux.E = @enumFromInt(-1 * msg.err);
+    if (code != .SUCCESS) {
+        return error.Error;
+    }
 }
