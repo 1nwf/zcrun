c7a2da0b7ea88aa762c43023897ef989f1bdb4cc
Nawaf <nwf.aloufi@gmail.com>
2024-03-07 22:30:15 -0500

    (rtnetlink) add link create

---
+++ src/rtnetlink/link/add.zig
@@ -0,0 +1,33 @@
+const LinkMessage = @import("link.zig");
+const RtNetLink = @import("../rtnetlink.zig");
+const std = @import("std");
+const linux = std.os.linux;
+
+const LinkAdd = @This();
+
+msg: LinkMessage,
+nl: *RtNetLink,
+pub fn init(allocator: std.mem.Allocator, nl: *RtNetLink) LinkAdd {
+    const msg = LinkMessage.init(allocator, .create);
+    return .{ .msg = msg, .nl = nl };
+}
+
+pub fn name(self: *LinkAdd, val: []const u8) !void {
+    try self.msg.addAttr(.{ .name = val });
+}
+
+pub fn veth(self: *LinkAdd, if_name: []const u8, peer_name: []const u8) !void {
+    try self.name(if_name);
+
+    var peer_info = LinkMessage.LinkInfo.init(self.msg.allocator);
+    try peer_info.attrs.append(.{ .name = peer_name });
+
+    try self.msg.addAttr(.{
+        .link_info = .{ .info = .{ .peer_info = peer_info }, .kind = .veth },
+    });
+}
+
+pub fn exec(self: *LinkAdd) !void {
+    try self.nl.send(try self.msg.compose());
+    return self.nl.recv_ack();
+}
