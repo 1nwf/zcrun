7946c56f948d219b5ba1648d4a8599c8633f250a
Nawaf <nwf.aloufi@gmail.com>
2024-03-08 21:27:09 -0500

    create new namespaces and setup mounts

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,37 @@
 const std = @import("std");
+const log = std.log;
+const linux = std.os.linux;
+const NetLink = @import("rtnetlink/rtnetlink.zig");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const args = std.os.argv;
+    if (args.len < 2) {
+        log.info("missing rootfs path", .{});
+        return;
+    }
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const rootfs = std.mem.span(args[1]);
+    if (args.len > 2 and std.mem.eql(u8, std.mem.span(args[2]), "child")) {
+        return childfn(rootfs);
+    }
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const err = linux.unshare(linux.CLONE.NEWNS | linux.CLONE.NEWPID | linux.CLONE.NEWUTS);
+    const code = linux.getErrno(err);
+    if (code != .SUCCESS) {
+        @panic("unshare err");
+    }
 
-    try bw.flush(); // don't forget to flush!
+    var child = std.ChildProcess.init(&.{ "/proc/self/exe", rootfs, "child" }, std.heap.page_allocator);
+    _ = try child.spawnAndWait();
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn childfn(rootfs: []const u8) !void {
+    _ = linux.chroot(@ptrCast(rootfs));
+    _ = linux.chdir("/");
+    _ = linux.mount("proc", "proc", "proc", 0, 0);
+
+    const name = "container";
+    _ = linux.syscall2(.sethostname, @intFromPtr(&name[0]), name.len);
+
+    std.process.execv(std.heap.page_allocator, &.{"/bin/bash"}) catch return error.ExecErr;
 }
