2bcd703e5627e883e9844a909c9eb916f27c76e4
Nawaf <nwf.aloufi@gmail.com>
2024-03-12 07:26:29 -0400

    fix minor memory bug

--- src/rtnetlink/link/get.zig
+++ src/rtnetlink/link/get.zig
@@ -8,14 +8,21 @@ const linux = std.os.linux;
 const LinkGet = @This();
 pub const Options = struct {
     name: ?[]const u8 = null,
+    index: ?u32 = null,
 };
 
 msg: LinkMessage,
 nl: *RtNetLink,
 opts: Options,
+allocator: std.mem.Allocator,
 pub fn init(allocator: std.mem.Allocator, nl: *RtNetLink, options: Options) LinkGet {
     const msg = LinkMessage.init(allocator, .get);
-    return .{ .msg = msg, .nl = nl, .opts = options };
+    return .{
+        .msg = msg,
+        .nl = nl,
+        .opts = options,
+        .allocator = allocator,
+    };
 }
 
 fn name(self: *LinkGet, value: []const u8) !void {
@@ -25,6 +32,9 @@ fn name(self: *LinkGet, value: []const u8) !void {
 fn applyOptions(self: *LinkGet) !void {
     if (self.opts.name) |val| {
         try self.name(val);
+    }
+    if (self.opts.index) |val| {
+        self.msg.msg.header.index = @intCast(val);
     }
 }
 
@@ -63,7 +73,9 @@ fn recv(self: *LinkGet) !LinkMessage {
         const rtattr = std.mem.bytesAsValue(linux.rtattr, buff[start .. start + @sizeOf(linux.rtattr)]);
         switch (rtattr.type) {
             .IFNAME => {
-                const ifname = buff[start + @sizeOf(linux.rtattr) .. start + rtattr.len - 1]; // skip null terminating byte
+                const value = buff[start + @sizeOf(linux.rtattr) .. start + rtattr.len - 1]; // skip null terminating byte
+                const ifname = try self.allocator.alloc(u8, value.len);
+                @memcpy(ifname, value);
                 log.info("name: {s}", .{ifname});
                 try link_info.addAttr(.{ .name = ifname });
             },
