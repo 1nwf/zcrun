9e4987fc34d47e05328e65084f4d6f68ee2836b6
Robert Kroeger <rjkroege@liqui.org>
2024-09-28 14:59:44 +0200

    Fix the build for Zig .13

--- build.zig
+++ build.zig
@@ -21,7 +21,7 @@ pub fn build(b: *std.Build) !void {
 
     const exe = b.addExecutable(.{
         .name = "zcrun",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path( "src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -56,7 +56,7 @@ pub fn build(b: *std.Build) !void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path( "src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
